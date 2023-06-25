70ae3a6d668b0a4dc2b4350f83effe75fcfd433b
Rudolph Reid <58621197+SanFran1@users.noreply.github.com>
2023-06-24 22:56:12 -0400

    implemented search function

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -2,6 +2,11 @@
 
 int search(int numbers[], int low, int high, int value) 
 {
+int mid = l + (r - l)/2;
+if (arr[mid] == x) return mid;
+if (arr[mid] > x) return binarySearch(arr, l, mid-1, x);
+return binarySearch(arr, mid+1, r, x);
+}
 	return -1;
 }
 
